7f302ed74174578dc0256ff6eeacb93f829480c7
O. William McClung <mcclung@nebrwesleyan.edu>
2016-12-08 16:05:43 -0600

    Add db.js

---
+++ db.js
@@ -0,0 +1,86 @@
+var categoryNames = [
+  "Was machst du?",
+  "Berühmte Deutschsprechende",
+  "Ein Ort",
+  "Speisen und Getränke",
+  "Sehenswürdigkeiten",
+];
+
+var category = [];
+
+category[0] = [
+  "Hausaufgaben machen",
+  "Mit dem Hund Gassi gehen",
+  "Bier trinken",
+  "Spaziergang machen",
+  "sich entspannen",
+  "Musik hören",
+  "im Internet surfen",
+  "faulenzen",
+  "Bücher lesen",
+  "Schlittschuh laufen",
+  "mit Freunden ausgehen",
+  "eine Pinkelpause machen"
+];
+
+category[1] = [
+  "Friedrich Nietzsche",
+  "Sigmund Freud",
+  "Angela Merkel",
+  "Clara Schumann",
+  "Ludwig Van Beethoven",
+  "Marlene Dietrich",
+  "Leni Riefenstahl",
+  "Richard Wagner",
+  "Martin Luther",
+  "Heidi Klum",
+  "Willy Brandt",
+  "Michael Schumacher",
+  "Maria Theresia",
+  "Wolfgang Amadeus Mozart",
+  "Johann Sebastian Bach",
+  "Karl der Große",
+  "Janosch",
+  "Til Schweiger"
+];
+
+category[2] = [
+  "Nürnberg",
+  "Hamburg",
+  "Lübeck",
+  "Baden-Baden",
+  "Der Schwarzwald",
+  "Der Biergarten",
+  "Der Flughafen",
+  "Der Hauptbahnhof",
+  "Die Haltestelle",
+  "Die Kunsthalle",
+  "Das Rathaus"
+];
+
+category[3] = [
+  "Pommes",
+  "Döner",
+  "Wurstsalat",
+  "Radler",
+  "Hefeweizen",
+  "Maultaschen",
+  "Weiẞbier",
+  "Weiẞwurst",
+  "Gulasch",
+  "Meeresfrüchte",
+  "Gummibären",
+];
+
+category[4] = [
+  "Kaiser-Wilhelm-Gedächtnis-Kirche",
+  "Schloss Neuschwanstein",
+  "Kölner Dom",
+  "Brandenburger Tor",
+  "Berliner Mauer",
+  "Schloss Schönbrunn",
+  "Kurfürstendamm",
+  "Wattenmeer",
+  "Fachwerkhäuser",
+  "Oktoberfest"
+];
